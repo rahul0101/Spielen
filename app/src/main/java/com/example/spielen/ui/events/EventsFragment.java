2953246cc83d626a8c9db99cce8bcbfe99c8b9e4
Sanchit Arora <sanchit@localhost.localdomain>
2020-03-04 18:01:49 +0530

    Add Profile Activity UI

---
+++ app/src/main/java/com/example/spielen/ui/events/EventsFragment.java
@@ -0,0 +1,32 @@
+package com.example.spielen.ui.events;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.spielen.R;
+
+public class EventsFragment extends Fragment {
+
+    private EventsViewModel eventsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        eventsViewModel =
+                ViewModelProviders.of(this).get(EventsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_events, container, false);
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        eventsViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        return root;
+    }
+}
--- app/src/main/java/com/example/spielen/ui/events/EventsViewModel.java
+++ app/src/main/java/com/example/spielen/ui/events/EventsViewModel.java
@@ -1,14 +1,14 @@
-package com.example.spielen.ui.home;
+package com.example.spielen.ui.events;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class EventsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public EventsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
