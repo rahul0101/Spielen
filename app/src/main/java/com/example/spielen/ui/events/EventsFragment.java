bf4d5e619f8fdfec381c9e533e1153d2a9e66da9
rahul <rahul.arcota@gmail.com>
2020-03-06 16:47:04 +0530

    Maps intent

--- app/src/main/java/com/example/spielen/ui/events/EventsFragment.java
+++ app/src/main/java/com/example/spielen/ui/events/EventsFragment.java
@@ -18,16 +18,25 @@ import com.example.spielen.EventAdapter;
 import com.example.spielen.JoinActivity;
 import com.example.spielen.R;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
 
 public class EventsFragment extends Fragment {
 
     private EventsViewModel eventsViewModel;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference eventsRef = db.collection("events");
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String id;
 
     private EventAdapter adapter;
 
@@ -59,11 +68,28 @@ public class EventsFragment extends Fragment {
         adapter.setOnItemClickListener(new EventAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
-                String id = documentSnapshot.getId();
-                Toast.makeText(getContext(), id, Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getContext(), JoinActivity.class);
-                intent.putExtra("id", id);
-                startActivity(intent);
+                id = documentSnapshot.getId();
+                //Toast.makeText(getContext(), id, Toast.LENGTH_SHORT).show();
+                eventsRef.document(id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful()) {
+                            DocumentSnapshot doc = task.getResult();
+                            if(doc.exists()) {
+                                    ArrayList arrayList = (ArrayList) doc.getData().get("players");
+                                    int x = arrayList.size();
+                                    if(x+1 == Integer.parseInt(doc.getData().get("size").toString())) {
+                                        Toast.makeText(getContext(), "Event Full!", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        Intent intent = new Intent(getContext(), JoinActivity.class);
+                                        intent.putExtra("id", id);
+                                        startActivity(intent);
+                                    }
+                            }
+                        }
+                    }
+                });
             }
         });
         return root;
