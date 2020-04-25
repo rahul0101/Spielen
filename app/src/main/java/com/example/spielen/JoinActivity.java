bc65c4e432fa10b4f3c7a445755baf4394a05c24
rahul <rahul.arcota@gmail.com>
2020-04-25 18:42:21 +0530

    Updated JoinActivity

--- app/src/main/java/com/example/spielen/JoinActivity.java
+++ app/src/main/java/com/example/spielen/JoinActivity.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -19,25 +21,66 @@ import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class JoinActivity extends AppCompatActivity {
 
     Button join,leave;
+    TextView tv2,tv3,tv4,tv5,tv6;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
     String id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_join);
 
         join = findViewById(R.id.buttonJoin);
         leave = findViewById(R.id.buttonLeave);
+        leave.setVisibility(View.GONE);
+        tv2 = findViewById(R.id.textView2);
+        tv3 = findViewById(R.id.textView3);
+        tv4 = findViewById(R.id.textView4);
+        tv5 = findViewById(R.id.textView5);
+        tv6 = findViewById(R.id.textView6);
+
+        final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("EEE dd/MM/yyyy");
+
 
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
+
+        rootRef.collection("events").document(id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                DocumentSnapshot doc = task.getResult();
+                if(doc.exists()) {
+                    Timestamp ts = (Timestamp) doc.getData().get("time");
+                    Date date = ts.toDate();
+                    ArrayList arrayList = (ArrayList) doc.getData().get("players");
+                    int x = arrayList.size() + 1;
+                    rootRef.collection("user_data").document(doc.getData().get("host").toString()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            DocumentSnapshot sd = task.getResult();
+                            if(sd.exists()) {
+                                tv2.setText(sd.getData().get("name").toString());
+                            }
+                        }
+                    });
+                    tv3.setText(doc.getData().get("name").toString());
+                    tv4.setText(dateFormat.format(date) + "  " + timeFormat.format(date));
+                    tv5.setText("Players: "+x+ " / "+ doc.getData().get("size"));
+
+                }
+            }
+        });
+
+
 
         join.setOnClickListener(new View.OnClickListener() {
             @Override
