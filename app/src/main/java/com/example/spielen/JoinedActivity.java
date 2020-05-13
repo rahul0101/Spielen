046e6f52becc812130207fb24d771bcba755d66a
rahul <rahul.arcota@gmail.com>
2020-05-13 10:51:03 +0530

    Added call, sms and whatsapp host

--- app/src/main/java/com/example/spielen/JoinedActivity.java
+++ app/src/main/java/com/example/spielen/JoinedActivity.java
@@ -19,9 +19,10 @@ import com.google.firebase.firestore.GeoPoint;
 
 public class JoinedActivity extends AppCompatActivity {
 
-    Button buttonMaps;
+    Button buttonMaps,buttonWhatsapp, buttonMessage, buttonCall;
     String id;
     Double lat,lon;
+    String phone="";
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
 
@@ -35,6 +36,10 @@ public class JoinedActivity extends AppCompatActivity {
         lat=0.0; lon=0.0;
 
         buttonMaps = findViewById(R.id.buttonMaps);
+        buttonCall = findViewById(R.id.btnCall);
+        buttonMessage = findViewById(R.id.btnMessage);
+        buttonWhatsapp = findViewById(R.id.btnWhatsapp);
+
 
         rootRef.collection("events").document(id).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -42,6 +47,22 @@ public class JoinedActivity extends AppCompatActivity {
                 if(task.isSuccessful()) {
                     DocumentSnapshot doc = task.getResult();
                     if(doc.exists()) {
+                        rootRef.collection("user_data").document(doc.getData().get("host").toString()).get().addOnCompleteListener(
+                                new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                        if(task.isSuccessful()) {
+                                            DocumentSnapshot ds = task.getResult();
+                                            if(ds.exists()) {
+                                                phone = ds.getData().get("phone").toString();
+                                                buttonCall.setEnabled(true);
+                                                buttonMessage.setEnabled(true);
+                                                buttonWhatsapp.setEnabled(true);
+                                            }
+                                        }
+                                    }
+                                }
+                        );
                         GeoPoint geoPoint = (GeoPoint) doc.getData().get("location");
                         lat = geoPoint.getLatitude();
                         lon = geoPoint.getLongitude();
@@ -59,6 +80,45 @@ public class JoinedActivity extends AppCompatActivity {
                 mapIntent.setPackage("com.google.android.apps.maps");
                 startActivity(mapIntent);
 
+            }
+        });
+
+        buttonWhatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String text = "";// Replace with your message.
+
+                    String toNumber = "91"+phone; // Replace with mobile phone number without +Sign or leading zeros, but with country code
+                    //Suppose your country is India and your phone number is “xxxxxxxxxx”, then you need to send “91xxxxxxxxxx”.
+
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("http://api.whatsapp.com/send?phone="+toNumber +"&text="+text));
+                    startActivity(intent);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        buttonMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setData(Uri.parse("smsto:91" + phone));
+                startActivity(intent);
+            }
+        });
+
+        buttonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:91"+phone));
+                startActivity(intent);
             }
         });
     }
