51212820774541988bdcb03b2047e5cc920cadcd
rahul <rahul.arcota@gmail.com>
2020-05-15 09:43:02 +0530

    Removed back button in JoinedEvents

--- app/src/main/java/com/example/spielen/ui/joined/JoinedViewModel.java
+++ app/src/main/java/com/example/spielen/ui/joined/JoinedViewModel.java
@@ -1,7 +1,18 @@
 package com.example.spielen.ui.joined;
 
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 public class JoinedViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> mText;
+
+    public JoinedViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is joined events fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
